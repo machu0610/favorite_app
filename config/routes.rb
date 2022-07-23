66d6df2ca9a8594e9519dae20502cfa64abdfc69
machu0610 <88845370+machu0610@users.noreply.github.com>
2022-07-23 18:45:12 +0900

    recipes controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/new'
+  get 'recipes/show'
+  get 'recipes/edit'
   root to: 'home#top' #rootパスでhomeに画面遷移
   devise_for :users
   resource :user, except: [:new, :create, :destroy]
