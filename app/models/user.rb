457faca63fae32c5ebdf6d7ef2d3517072a2d359
machu0610 <88845370+machu0610@users.noreply.github.com>
2022-07-24 00:06:56 +0900

    いいねを判断するためのメソッド作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
 
   has_many :resipes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  def already_favorited?(recipe) #いいねの判定をするためのメソッド(自作メソッド)
+    self.favorites.exists?(recipe_id: recipe.id)
+    #コードの説明
+    #selfにはcurrent_userが入る。
+    #crrent_userに結びついているいいねの中で、recipe_idが今いいねをしようとしているレシピのID recipe.idが存在するかを調べている
+  end
 end
