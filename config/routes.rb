19211a1ec1e2363f72f9e91cfbc6a65ef182423f
machu0610 <88845370+machu0610@users.noreply.github.com>
2022-07-24 00:12:55 +0900

    ネストしたルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#top' #rootパスでhomeに画面遷移
   devise_for :users
   resource :user, except: [:new, :create, :destroy]
-  resources :recipes
+  resources :recipes do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
