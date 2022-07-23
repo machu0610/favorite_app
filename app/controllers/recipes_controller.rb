c6d489388bf337875ee15233c45e404f447381e4
machu0610 <88845370+machu0610@users.noreply.github.com>
2022-07-23 19:01:32 +0900

    recipes controllerのindex,new,create,show作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,24 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id #ログインユーザーのIDを@recipeのuser_id(カラム)に代入
+    if @recipe.save #もし保存されたら
+      redirect_to recipe_path(@recipe),notice: '投稿に成功しました。' #リダイレクトで投稿したレシピの詳細画面へ遷移する
+    else
+      render :new
+    end
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def edit
