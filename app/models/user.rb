fd8792bc63f97df1bbea0da8ed7dd4f388979c41
machu0610 <88845370+machu0610@users.noreply.github.com>
2022-07-23 19:28:06 +0900

    user modelにアソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+
+  has_many :resipes, dependent: :destroy
 end
