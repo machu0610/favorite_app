0f3565cf9a158317ec16b1c44516e6e4dd5d3485
machu0610 <88845370+machu0610@users.noreply.github.com>
2022-07-25 09:58:09 +0900

    バリデーション追加

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
 
-  validates_uniquenss_of :recipe_id, scope: :user_id #この書き方でrecipe_idとuser_idの組みが一通りしかないことができる
+  validates_uniqueness_of :recipe_id, scope: :user_id #この書き方でrecipe_idとuser_idの組みが一通りしかないことができる
 end
